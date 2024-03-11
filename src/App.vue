31b89a38f0b2ae14f4bb3867685068b42c026def
Shohzod <xorozovshohzod@gmail.com>
2024-03-11 05:44:52 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import HomeViewVue from "./views/HomeView.vue";
+</script>
+
+<template>
+  <section>
+    <RouterView />
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <div class="container"></div>
+  </section>
+</template>
